502b34d278979f52a3a1b41d8346a9d668600afa
=Engin Gülek <=engingulek0@gmail.com>
2021-02-25 20:57:55 +0300

    Revize

--- glk-comp/src/firebase.js
+++ glk-comp/src/firebase.js
@@ -1,17 +1,1 @@
-import firebase from "firebase"
-const firebaseConfig = {
-    apiKey: "AIzaSyANWAnao19TmZOD8D7_s5AWYbbHViiT8VI",
-    authDomain: "glk-comp.firebaseapp.com",
-    projectId: "glk-comp",
-    storageBucket: "glk-comp.appspot.com",
-    messagingSenderId: "1029640029494",
-    appId: "1:1029640029494:web:d7fd51f58042cd2c4e1f97",
-    measurementId: "G-GF61YPH1NR"
-  };
-
-  const firebaseApp =firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore()
-const auth = firebase.auth();
-const providerGoogle = new firebase.auth.GoogleAuthProvider();
-export {auth,providerGoogle};
-export default db;
+x
