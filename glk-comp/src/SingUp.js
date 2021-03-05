9c3d6044ac8c38da24b27d2539bb6684df4d167c
Engin Gülek <engingulek0@gmail.com>
2021-03-05 22:47:03 +0300

    SingUpCreateAccount->Firebase

--- glk-comp/src/SingUp.js
+++ glk-comp/src/SingUp.js
@@ -1,6 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./SingUp.css"
+import db from "./firebase"
+import alert from "alertifyjs"
 function SingUp() {
+  const [accountnameSurname,setAccountnameSurname]=useState("")
+  const [accountemail,setAccountemail]=useState("")
+  const [accountpassword,setAccountpassword]=useState("")
+  const [accountpasswordAgain,setAccountpasswordAgain]=useState("")
+
+  const createAccount =(e)=>{
+    e.preventDefault()
+
+    
+
+    if (accountpassword===accountpasswordAgain) {
+      if(accountnameSurname==="" ||accountemail===""||accountpassword===""|| accountpasswordAgain==="")  {
+        alert.error("Fill in the empty")
+      }
+
+      else(
+        db.collection("account").doc(accountnameSurname).set({
+          nameSurname:accountnameSurname,
+          email:accountemail,
+          password:accountpassword,
+        })
+      )
+    }
+    
+    else(
+      alert.error("Match password do not")
+      
+    )
+    
+  }
+
+  const nameSurname=(namesurname)=>{
+    
+    setAccountnameSurname(namesurname)
+  }
+  const email=(emaill)=>{
+    
+    setAccountemail(emaill)
+  }
+  const password=(passwordd)=>{
+    
+    setAccountpassword(passwordd)
+  }
+  const passwordAgain=(passwordagain)=>{
+    
+    setAccountpasswordAgain(passwordagain)
+  }
+
+
     return (
         <div className="singIn__container">
         <div className="singIn">
@@ -9,20 +60,20 @@ function SingUp() {
             <form>
               <div className="nameSurname">
                 <label>Ad-Soyad</label>
-                <input type="text" />
+                <input type="text" onChange={(e)=>nameSurname(e.target.value)} />
               </div>
               <div className="eposta">
                 <label>E-posta</label>
-                <input type="text" />
+                <input type="text" onChange={(e)=>email(e.target.value)} />
               </div>
               <div className="password">
                 
                   <label>Password</label>
-                  <input type="text" />
+                  <input type="password" onChange={(e)=>password(e.target.value)} />
                 
                 
                   <label>Password Again</label>
-                  <input type="text" />
+                  <input type="password" onChange={(e)=>passwordAgain(e.target.value)} />
                 
               </div>
               <div className="termsofUse">
@@ -33,12 +84,10 @@ function SingUp() {
               </div>
               
             </div>
-              <div className="createButton">
-                <button>Create Account</button>
+              <div className="createButton bttn">
+                <button onClick={createAccount}>Create Account</button>
               </div>
-              <div className="GoogleSignIn">
-            <button>Google Giris</button>
-          </div>
+            
               
             </form>
             
