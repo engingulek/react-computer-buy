84f2364fc3b5a7a59183e02b75c2c4bac449941a
Engin Gülek <engingulek0@gmail.com>
2021-03-06 15:58:34 +0300

    Finish

--- glk-comp/src/Information.js
+++ glk-comp/src/Information.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {useState } from "react";
 import db from "./firebase"
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import "./Information.css"
